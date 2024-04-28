6c548a29cacb41497917a0a7502bb7c893137131
Anniel <annielfranciscoreyes@gmail.com>
2024-04-28 17:37:34 -0400

    chore(valorant): change in Home and CardHome code

--- src/components/cards/CardHome.vue
+++ src/components/cards/CardHome.vue
@@ -1,6 +1,51 @@
 <template>
-  <div></div>
+  <div class="card cursor-pointer" @click="goToPage(route)">
+    {{ name }}
+  </div>
 </template>
 
-<script></script>
-<style scoped></style>
+<script>
+export default {
+  name: "CardHome",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    route: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    goToPage(route) {
+      this.$router.push({ path: route });
+    },
+  },
+};
+</script>
+<style scoped>
+.card {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 600px;
+  height: 250px;
+  align-items: center;
+  outline: none;
+  color: white;
+  text-transform: uppercase;
+  font-weight: 500;
+}
+
+.card:hover {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+@media (max-width: 768px) {
+  .card {
+    width: 500px;
+  }
+}
+</style>
